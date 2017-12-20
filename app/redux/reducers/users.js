94e2847bd030c323046331b4b70de080e91f7d9c
Yugaank Sharma <yugaanks@yahoo.co.in>
2017-12-19 19:28:20 -0500

    feed and login layout, users function

---
+++ app/redux/reducers/users.js
@@ -0,0 +1,80 @@
+export const addUser = user => dispatch => {
+  let data = new FormData();
+  data.append('media', {
+        uri: user.image,
+        type: 'image/jpeg',
+        name: 'image'
+      });
+  return axios.post(`https://api.cosmicjs.com/v1/${cosmicConfig.bucket.slug}/media`, data)
+  .then(res => res.data.media)
+  .then(media => {
+    return axios.post(`https://api.cosmicjs.com/v1/${cosmicConfig.bucket.slug}/add-object`, {
+      title: user.firstName + ' ' + user.lastName,
+      type_slug: 'users',
+      metafields: [
+        {
+          key: 'name',
+          type: 'text',
+          value: user.firstName + ' ' + user.lastName,
+        },
+        {
+          key: 'username',
+          type: 'text',
+          value: user.username,
+        },
+        {
+          key: 'password',
+          type: 'text',
+          value: user.password,
+        },
+        {
+          key: 'profile_picture',
+          type: 'file',
+          value: media.name,
+              }
+            ]
+          }
+        )}
+      )
+      .then(res => formatUser(res.data))
+      .then(formattedUser => dispatch(createUser(formattedUser)))
+      .then(() => Actions.feed())
+      .catch(err => console.error(`Creating user unsuccessful`, err))
+}
+
+export const authenticate = user => dispatch => {
+  return axios.get(`https://api.cosmicjs.com/v1/${cosmicConfig.bucket.slug}/object-type/users/search?metafield_key=username&metafield_value=${user.username}`)
+    .then(res => res.data)
+    .then(data => {
+      console.log('RESPONSE: ', data);
+      if (data.objects) {
+        const userData = data.objects[0];
+        return {
+          password: userData.metadata.password,
+          username: userData.metadata.username,
+          name: userData.metadata.name,
+          profilePicture: userData.metadata.profile_picture,
+          slug: userData.slug,
+          id: userData._id,
+        }
+      } else {
+        return 'Username invalid';
+      }
+    })
+    .then(data => {
+      if (data === 'Username invalid'){
+        return data;
+      } else if (data.password === user.password){
+        dispatch(login({
+          name: data.name,
+          username: data.username,
+          profilePicture: data.profilePicture,
+          slug: data.slug,
+          id: data.id,
+        }))
+      } else {
+        return 'Password invalid';
+      }
+    })
+    .catch(error => console.error('Login unsuccessful', error))
+}
